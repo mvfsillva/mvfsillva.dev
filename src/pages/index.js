ce8f1ab543e4bca56a472eab13e5df8d8469e758
mvfsilva <mvfsillva@gmail.com>
2019-03-05 05:50:32 -0300

    refactor: add lion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 32px;
+import lion from '../static/images/lion.svg'
+
+const Lion = styled.div`
+  background-image: url(${lion});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
 `
-const IndexPage = () => <Title>Marcus Silva</Title>
+
+const IndexPage = () => <Lion />
 
 export default IndexPage
