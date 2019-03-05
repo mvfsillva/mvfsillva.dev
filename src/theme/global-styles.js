54efe1d835bef43e4dfa8bb40bd2dba7eb75ed70
mvfsilva <mvfsillva@gmail.com>
2019-03-05 18:25:28 -0300

    add global styles

---
+++ src/theme/global-styles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Open Sans, Helvetica Neue, Helvetica, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    &::after,
+    &::before {
+      box-sizing: inherit;
+    }
+  }
+  html {
+    box-sizing: border-box;
+    font-family: sans-serif;
+    text-size-adjust: 100%;
+    background: #ffffff;
+  }
+  a {
+    text-decoration: none;
+  }
+`
+
+export default GlobalStyles
