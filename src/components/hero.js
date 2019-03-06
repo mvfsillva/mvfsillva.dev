893dbf9db1e0763f07282f8d090a394990bb61a8
mvfsilva <mvfsillva@gmail.com>
2019-03-06 03:18:23 -0300

    minor: add hero component

---
+++ src/components/hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Lion from '../icons/lion'
+
+const Wrapper = styled.header`
+  position: relative;
+  width: 100%;
+  color: ${({ theme }) => theme.palette.black};
+  padding: ${({ theme }) => theme.spacing.medium};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+  max-width: 980px;
+  padding: ${({ theme }) => theme.spacing.large};
+`
+
+const Hero = () => (
+  <Wrapper>
+    <Container>
+      <Lion />
+    </Container>
+  </Wrapper>
+)
+
+export default Hero
