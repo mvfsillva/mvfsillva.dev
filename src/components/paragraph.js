3bb307995b1bb992ef7d911eeaa7e74bf0b8458f
mvfsilva <mvfsillva@gmail.com>
2019-03-10 19:20:58 -0300

    patch: adjustment in typography

--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 
 const Paragraph = styled.p`
+  ${({ theme }) => theme.typography.paragraph};
   color: ${({ theme }) => theme.palette.black};
-  font-size: ${({ theme }) => theme.typography.paragraph.fontSize};
-  font-weight: ${({ theme }) => theme.typography.paragraph.fontWeight};
 `
 
 export default Paragraph
