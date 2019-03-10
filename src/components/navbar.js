496ecb37b500dc899500d4ccc2ba0a817b25c9ed
Marcus Silva <mvfsillva@gmail.com>
2019-03-10 19:17:37 -0300

    minor: component-navbar (#5)
    * add navbar component
    
    * add shortid pkg

---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import shortid from 'shortid'
+import { Link } from 'gatsby'
+import { transparentize, transitions } from 'polished'
+
+import { transition } from '../mixins/transition'
+
+const Nav = styled.nav`
+  display: flex;
+  list-style: none;
+  justify-content: center;
+  align-items: center;
+  a {
+    ${({ theme }) => theme.typography.link};
+    color: ${({ theme }) => theme.palette.black};
+    margin: ${({ theme }) => theme.spacing};
+    ${transitions(transition({ property: 'color', duration: '250ms' }))};
+    &:hover {
+      color: ${({ theme }) => transparentize(0.5, `${theme.palette.black}`)};
+      ${transitions(transition({ property: 'color', duration: '250ms' }))};
+    }
+  }
+`
+
+const Navbar = ({ navigation }) => (
+  <Nav>
+    {navigation.map(url => (
+      <Link key={shortid.generate()} to={`/${url}`}>
+        {url}
+      </Link>
+    ))}
+  </Nav>
+)
+
+Navbar.defaultProps = {
+  navigation: [],
+}
+
+Navbar.propTypes = {
+  navigation: PropTypes.arrayOf(PropTypes.string),
+}
+
+export default Navbar
