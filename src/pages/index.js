c5a78b85a330c8e089f377958cc0e17bd7332ce1
Marcus Silva <mvfsillva@gmail.com>
2019-03-10 19:21:23 -0300

    patch: adjustment in typography (#7)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Main from '../layouts/main'
-
+import Content from '../styles/content'
 import Hero from '../components/hero'
 
 const Intro = styled.h1`
-  font-size: ${({ theme }) => theme.typography.intro.fontSize};
-  font-weight: ${({ theme }) => theme.typography.intro.fontWeight};
-  letter-spacing: 6px;
-  font-family: Open Sans;
+  ${({ theme }) => theme.typography.intro};
 `
 
 const SubTitle = styled.h2`
-  font-size: ${({ theme }) => theme.typography.subtitle.fontSize};
-  font-weight: ${({ theme }) => theme.typography.subtitle.fontWeight};
-  letter-spacing: 6px;
+  ${({ theme }) => theme.typography.subtitle};
   margin-top: ${({ theme }) => theme.spacing.large};
 `
 
@@ -25,17 +20,6 @@ const Hgroup = styled.hgroup`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`
-
-const Content = styled.main`
-  width: 100%;
-  max-width: 1400px;
-  padding: ${({ theme }) => theme.spacing.xxxLarge};
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-  justify-content: center;
-  flex: 1 0 auto;
 `
 
 const IndexPage = () => (
