2519d6be7b2c9b14e1579d72abbd045ac62a596b
mvfsilva <mvfsillva@gmail.com>
2019-03-10 21:43:56 -0300

    patch: add spacing

--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Paragraph = styled.p`
   ${({ theme }) => theme.typography.paragraph};
   color: ${({ theme }) => theme.palette.black};
+  padding: ${({ theme }) => theme.spacing};
 `
 
 export default Paragraph
