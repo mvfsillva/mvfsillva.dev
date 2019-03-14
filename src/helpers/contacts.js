4632abfa2856b6bfe855b550e90520a08464082b
Marcus Silva <mvfsillva@gmail.com>
2019-03-14 16:27:00 -0300

    minor: add contatcs helper (#10)

---
+++ src/helpers/contacts.js
@@ -0,0 +1,16 @@
+const contacts = [
+  {
+    name: 'twitter',
+    link: 'https://twitter.com/mvfsillva',
+  },
+  {
+    name: 'github',
+    link: 'https://github.com/mvfsillva',
+  },
+  {
+    name: 'linkedin',
+    link: 'https://linkedin.com/in/mvfsilva',
+  },
+]
+
+export default contacts
