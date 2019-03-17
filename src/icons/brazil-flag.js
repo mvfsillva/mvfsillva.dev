6f721037947c8a02cb8a9ba1d27256e96352e9fa
mvfsilva <mvfsillva@gmail.com>
2019-03-17 16:32:23 -0300

    minor: add flags to en and ptbr lang

---
+++ src/icons/brazil-flag.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { invert } from 'polished'
+
+const BrazilFlag = ({ color, reverse, size }) => (
+  <svg width={size} height={size} viewBox="0 0 480 480">
+    <g>
+      <path
+        id="ptBr"
+        fill={reverse ? invert(color) : color}
+        d="M480,88H0v304h480V88z M240,381.1l-194-141l194-141l194,141L240,381.1z"
+      />
+      <path
+        id="ptBr"
+        fill={reverse ? invert(color) : color}
+        d="M174.8,174.6c57.5,17,110.4,49.3,151.7,92.801c2.7-8.601,4.1-17.801,4.1-27.301c0-50.6-41-91.7-91.7-91.7C214,148.4,191.3,158.4,174.8,174.6z"
+      />
+      <path
+        id="ptBr"
+        fill={reverse ? invert(color) : color}
+        d="M160.4,192.8c-8.3,13.8-13.1,30-13.1,47.3c0,50.6,41,91.7,91.7,91.7c32.9,0,61.7-17.3,77.9-43.399C275.1,242.199,220.6,208.8,160.4,192.8z"
+      />
+    </g>
+  </svg>
+)
+
+BrazilFlag.defaultProps = {
+  color: '#000',
+  size: 50,
+  reverse: false,
+}
+
+BrazilFlag.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.number,
+  reverse: PropTypes.bool,
+}
+
+export default BrazilFlag
---
+++ src/icons/usa-flag.js
@@ -0,0 +1,137 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { invert } from 'polished'
+
+const UsaFlag = ({ color, reverse, size }) => (
+  <svg width={size} height={size} viewBox="0 0 464 464">
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="256" y="96" width="176" height="16" />
+    </g>
+
+    <g>
+      <rect
+        id="en"
+        fill={reverse ? invert(color) : color}
+        x="256"
+        y="128"
+        width="176"
+        height="16"
+      />
+    </g>
+
+    <g>
+      <rect
+        id="en"
+        fill={reverse ? invert(color) : color}
+        x="256"
+        y="160"
+        width="176"
+        height="16"
+      />
+    </g>
+
+    <g>
+      <rect
+        id="en"
+        fill={reverse ? invert(color) : color}
+        x="256"
+        y="192"
+        width="176"
+        height="16"
+      />
+    </g>
+
+    <g>
+      <rect
+        id="en"
+        fill={reverse ? invert(color) : color}
+        x="256"
+        y="224"
+        width="176"
+        height="16"
+      />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="256" width="400" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="288" width="400" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="320" width="400" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="352" width="400" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="96" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="96" y="96" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="160" y="96" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="64" y="128" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="128" y="128" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="192" y="128" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="32" y="160" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="96" y="160" width="16" height="16" />
+    </g>
+
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="160" y="160" width="16" height="16" />
+    </g>
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="64" y="192" width="16" height="16" />
+    </g>
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="128" y="192" width="16" height="16" />
+    </g>
+    <g>
+      <rect id="en" fill={reverse ? invert(color) : color} x="192" y="192" width="16" height="16" />
+    </g>
+    <g>
+      <path
+        id="en"
+        fill={reverse ? 'none' : color}
+        d="M240,64H0v176v160h464V64H240z M16,80h208v144H16V80z M448,384H16V240h224V80h208V384z"
+      />
+    </g>
+  </svg>
+)
+
+UsaFlag.defaultProps = {
+  color: '#000',
+  size: 50,
+  reverse: false,
+}
+
+UsaFlag.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.number,
+  reverse: PropTypes.bool,
+}
+
+export default UsaFlag
