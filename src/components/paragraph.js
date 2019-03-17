87c614e5e43634c8987fa68aeca8c53f29365d94
mvfsilva <mvfsillva@gmail.com>
2019-03-17 16:34:38 -0300

    style: set max-width

--- src/components/paragraph.js
+++ src/components/paragraph.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { theme } from 'styled-tools'
 
 const Paragraph = styled.p`
+  max-width: 80ch;
   ${theme('typography.paragraph')};
   color: ${theme('palette.black')};
   margin-top: ${theme('spacing.medium')};
