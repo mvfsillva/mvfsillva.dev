1c01470387d16caa46ae921d62d7113638b71666
mvfsilva <mvfsillva@gmail.com>
2019-03-17 16:33:31 -0300

    minor: add i18n

---
+++ src/helpers/intl.js
@@ -0,0 +1,16 @@
+const intl = {
+  home: {
+    ptbr: {
+      intro: 'Olá, Sou Marcus',
+      sub: 'Desenvolvedor Front-End',
+      navigation: ['sobre', 'projetos', 'artigos'],
+    },
+    en: {
+      intro: "Hi, I'm Marcus",
+      sub: 'Front-End Developer',
+      navigation: ['about', 'projects', 'articles'],
+    },
+  },
+}
+
+export default intl
