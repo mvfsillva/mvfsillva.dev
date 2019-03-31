86fa754645d45935c4717058fdc49c81ba993b2d
mvfsilva <mvfsillva@gmail.com>
2019-03-31 16:47:25 -0300

    add about en language version

--- src/helpers/intl.js
+++ src/helpers/intl.js
@@ -39,16 +39,17 @@ const intl = {
       navigation: ['projects'],
       contacts: 'If you want to know more about me or get in touch',
       description: [
-        'Natural de Salvador/BA, cresci no bairro da Mata Escura, atualmente mostro todo dendê da Bahia em São Paulo/SP, e trabalho como desenvolvedor sênior Front-end na IDwall.',
-        'Já trabalhei em lugares como SENAI CIMATEC, 99 Táxis, Nextel (atualmente Claro) e algumas outras. Sou formado em desenvolvimento de sistemas, e também em Analise e desenvolvimento de sistemas pelo SENAI.',
+        'Born in Salvador/BA, I grew up in the neighborhood of Mata Escura, currently showing all Bahia dendê oil in São Paulo/SP, and I have been working as senior front-end developer on IDwall.',
+        "I've worked on places as SENAI CIMATEC, 99 Táxis, Nextel (currently Claro), and some others. I'm a degree in software development and also analysis and systems development by SENAI.",
+        'My main focus throughout my career has been web technologies and I love work with challenging and creative projects.',
       ],
       beyond: {
-        title: 'Não apenas de programação vive um mortal:',
+        title: "I'm not only about programming:",
         list: [
-          'Sou um estudioso curioso de história',
-          'Apaixonado por carnaval (O de Salvador é claro!!)',
-          'Dançarino nas horas vagas',
-          'Aquela saídera com os amigos por que ninguém é de ferro...',
+          "I'm a curious student of history",
+          'Passionate about carnival (the one from Salvador of course!!)',
+          'Dancer on my free time',
+          'That one leaves with friends because nobody is of iron.',
         ],
       },
     },
