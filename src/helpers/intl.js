98b0625651a6ec896f2fdffaed5607b7ce03875a
mvfsilva <mvfsillva@gmail.com>
2019-03-30 21:41:13 -0300

    minor: add description and beyond section

--- src/helpers/intl.js
+++ src/helpers/intl.js
@@ -1,7 +1,7 @@
 const intl = {
   general: {
     'pt-br': {
-      intro: 'Olá, Sou o Marcus',
+      intro: 'Olá, Sou Marcus',
     },
     en: {
       intro: "Hi, I'm Marcus",
@@ -21,10 +21,36 @@ const intl = {
     'pt-br': {
       navigation: ['projetos'],
       contacts: 'Se você quiser saber mais sobre mim ou entrar em contato',
+      description: [
+        'Natural de Salvador/BA, cresci no bairro da Mata Escura, atualmente mostro todo dendê da Bahia em São Paulo/SP, e trabalho como desenvolvedor sênior Front-end na IDwall.',
+        'Já trabalhei em lugares como SENAI CIMATEC, 99 Táxis, Nextel (atualmente Claro) e algumas outras. Sou formado em desenvolvimento de sistemas, e também em Analise e desenvolvimento de sistemas pelo SENAI.',
+      ],
+      beyond: {
+        title: 'Não apenas de programação vive um mortal:',
+        list: [
+          'Sou um estudioso curioso de história',
+          'Apaixonado por carnaval (O de Salvador é claro!!)',
+          'Dançarino nas horas vagas',
+          'Aquela saídera com os amigos por que ninguém é de ferro...',
+        ],
+      },
     },
     en: {
       navigation: ['projects'],
       contacts: 'If you want to know more about me or get in touch',
+      description: [
+        'Natural de Salvador/BA, cresci no bairro da Mata Escura, atualmente mostro todo dendê da Bahia em São Paulo/SP, e trabalho como desenvolvedor sênior Front-end na IDwall.',
+        'Já trabalhei em lugares como SENAI CIMATEC, 99 Táxis, Nextel (atualmente Claro) e algumas outras. Sou formado em desenvolvimento de sistemas, e também em Analise e desenvolvimento de sistemas pelo SENAI.',
+      ],
+      beyond: {
+        title: 'Não apenas de programação vive um mortal:',
+        list: [
+          'Sou um estudioso curioso de história',
+          'Apaixonado por carnaval (O de Salvador é claro!!)',
+          'Dançarino nas horas vagas',
+          'Aquela saídera com os amigos por que ninguém é de ferro...',
+        ],
+      },
     },
   },
 }
