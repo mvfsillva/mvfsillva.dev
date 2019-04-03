47345b61b8828b9a3fa22bff401edb04c53571fa
mvfsilva <mvfsillva@gmail.com>
2019-04-03 15:24:58 -0300

    refactor: add apollo provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import client from './src/services/client'
+
 require('typeface-playfair-display')
 require('typeface-open-sans')
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
