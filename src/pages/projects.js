0886a87e01a02e9b2d018b348cf947f59e6e2d25
mvfsilva <mvfsillva@gmail.com>
2019-04-05 18:27:03 -0300

    refactor: improve page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,41 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import Card from '../components/card'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import Loader from '../components/loader'
 
 import Content from '../styles/content'
 
 import contacts from '../helpers/contacts'
 import intl from '../helpers/intl'
 
-import Either from '../utils/either'
-
 import getProjects from '../queries/projects'
 
-const Projects = ({ lang, setLanguage }) => {
-  const [data, setData] = useState([])
-  const fetchCardList = async () => {}
-
-  useEffect(() => fetchCardList(), {})
-
-  getProjects()
-  return (
-    <>
-      <Header navigation={intl.general[lang].navigation} setLanguage={setLanguage} back />
-      <Content>
-        <Either
-          condition={data}
-          correct={<Card data={data} title={intl.projects[lang].title} />}
-          exception={<Loader />}
-        />
-      </Content>
-      <Footer center contacts={contacts} />
-    </>
-  )
-}
+const Projects = ({ lang, setLanguage }) => (
+  <>
+    <Header navigation={intl.general[lang].navigation} setLanguage={setLanguage} back />
+    <Content>
+      <Card data={getProjects()} title={intl.projects[lang].title} />
+    </Content>
+    <Footer center contacts={contacts} />
+  </>
+)
 
 Projects.propTypes = {
   lang: PropTypes.string.isRequired,
