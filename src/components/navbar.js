c926626843acdcad5b19674cc5d2c62cfa049ea6
mvfsilva <mvfsillva@gmail.com>
2019-04-06 16:38:04 -0300

    style: added the mobile version navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,9 +9,13 @@ import { theme, ifProp } from 'styled-tools'
 import { transition } from '../mixins/transition'
 
 import setRouting from '../helpers/set-routing'
+
 import If from '../utils/if'
 
+import Locale from './locale'
+
 const Nav = styled.nav`
+  width: 66.66%;
   display: flex;
   list-style: none;
   justify-content: center;
@@ -29,22 +33,133 @@ const Nav = styled.nav`
     }
   }
 
-  @media ${theme('responsive.phone')} {
-    display: none;
+  .nav-links {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 600px) {
+    .nav-links {
+      display: flex;
+      justify-content: flex-start;
+      position: absolute;
+      flex-direction: column;
+      background-color: ${({ theme }) => theme.palette.gray['800']};
+      width: 100%;
+      height: 100vh;
+      ${transitions(transition({ duration: '0.5s', ease: 'ease-in' }))};
+      overflow-y: hidden;
+      top: 0px;
+      left: 0px;
+    }
+
+    a {
+      color: ${theme('palette.white')};
+      text-align: center;
+      padding: ${theme('spacing')};
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0;
+      ${transitions(transition({ property: 'background', duration: '250ms' }))};
+      &:hover {
+        color: ${({ theme }) => transparentize(0.5, `${theme.palette.white}`)};
+        background-color: ${({ theme }) => transparentize(0.5, `${theme.palette.black}`)};
+        ${transitions(transition({ property: 'background', duration: '250ms' }))};
+      }
+    }
   }
 `
 
-const Navbar = ({ navigation, back, reverse }) => (
-  <Nav reverse={reverse}>
-    <If test={back}>
-      <Link to="/">Home</Link>
-    </If>
-    {navigation.map(router => (
-      <Link key={shortid.generate()} to={`/${setRouting(router).url}`}>
-        {setRouting(router).text}
-      </Link>
-    ))}
-  </Nav>
+const Sandwich = styled.div`
+  display: none;
+  @media (max-width: 600px) {
+    display: flex;
+    position: absolute;
+    right: ${theme('spacing.medium')};
+    z-index: ${theme('zindex.dropdown')};
+    cursor: pointer;
+  }
+`
+
+const Label = styled.label`
+  @media (max-width: 600px) {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    padding: ${theme('spacing.medium')};
+    &:hover {
+      background-color: ${({ theme }) => transparentize(0.5, `${theme.palette.black}`)};
+    }
+
+    span {
+      display: block;
+      width: 25px;
+      height: 10px;
+      border-top: 2px solid ${ifProp('reverse', theme('palette.white'), theme('palette.black'))};
+    }
+  }
+`
+
+const Check = styled.input`
+  display: none;
+  @media (max-width: 600px) {
+    &:checked ~ .nav-links {
+      overflow-y: auto;
+      padding-top: ${theme('spacing.xxxHuge')};
+    }
+    &:not(:checked) ~ .nav-links {
+      height: 0px;
+    }
+  }
+`
+
+const Content = styled.div`
+  width: 50%;
+  text-align: center;
+  @media (max-width: 600px) {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    a {
+      width: 100%;
+    }
+  }
+`
+
+const Navbar = ({ navigation, back, reverse, setLanguage }) => (
+  <>
+    <Nav reverse={reverse}>
+      <Check type="checkbox" id="check" />
+      <Sandwich>
+        <Label htmlFor="check" reverse={reverse}>
+          <span />
+          <span />
+          <span />
+        </Label>
+      </Sandwich>
+
+      <div className="nav-links">
+        <Content>
+          <If test={back}>
+            <Link to="/">Home</Link>
+          </If>
+          {navigation.map(router => (
+            <Link key={shortid.generate()} to={`/${setRouting(router).url}`}>
+              {setRouting(router).text}
+            </Link>
+          ))}
+        </Content>
+        <Locale reverse={reverse} setLanguage={setLanguage} />
+      </div>
+    </Nav>
+  </>
 )
 
 Navbar.defaultProps = {
@@ -55,6 +170,7 @@ Navbar.defaultProps = {
 
 Navbar.propTypes = {
   navigation: PropTypes.arrayOf(PropTypes.string),
+  setLanguage: PropTypes.func.isRequired,
   back: PropTypes.bool,
   reverse: PropTypes.bool,
 }
