7a908718d3f459105021703d91f632af9ee654f6
mvfsilva <mvfsillva@gmail.com>
2019-04-06 16:38:58 -0300

    refactor: improvement layout-component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,71 +13,71 @@ import lion from '../static/images/lion.png'
 
 import pkg from '../../package.json'
 
-  class Layout extends Component {
-    state = {
-      lang: 'en'
-    }
+class Layout extends Component {
+  state = {
+    lang: 'en'
+  }
 
-    static propTypes = {
-      children: PropTypes.node.isRequired,
-      location: PropTypes.object,
-    }
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    location: PropTypes.object,
+  }
 
-    componentDidMount() {
-      const lang = localStorage.getItem('language') || 'en'
+  componentDidMount() {
+    const lang = localStorage.getItem('language') || 'en'
+    this.setState({ lang })
+  }
+
+  render () {
+    const { lang } = this.state
+    const { children, location } = this.props
+    const { pathname } = location
+    const customSiteTitle = pathname !== '/' && `mvfsillva - ${pathname.replace('/', '')}`
+    const setLanguage = lang => {
+      localStorage.setItem('language', lang)
       this.setState({ lang })
     }
 
-    render () {
-      const { lang } = this.state
-      const { children, location } = this.props
-      const { pathname } = location
-      const customSiteTitle = pathname !== '/' && `mvfsillva - ${pathname.replace('/', '')}`
-      const setLanguage = lang => {
-        localStorage.setItem('language', lang)
-        this.setState({ lang })
-      }
+    const childrenWithSetLanguage = Children.map(children, child => cloneElement(child, { lang, setLanguage }))
 
-      const childrenWithSetLanguage = Children.map(children, child => cloneElement(child, { lang, setLanguage }))
-
-      return (
-        <Transition location={location}>
-          <StaticQuery
-            query={graphql`
-              query SiteTitleQuery {
-                site {
-                  siteMetadata {
-                    title
-                    siteUrl
-                    description
-                    author
-                  }
+    return (
+      <Transition location={location}>
+        <StaticQuery
+          query={graphql`
+            query SiteTitleQuery {
+              site {
+                siteMetadata {
+                  title
+                  siteUrl
+                  description
+                  author
                 }
               }
-            `}
-            render={({ site }) => (
-              <ThemeProvider theme={theme}>
-                <FullContainer>
-                  <Helmet title={customSiteTitle || site.siteMetadata.title} htmlAttributes={{ lang }}>
-                    <meta name="description" content={pkg.description} />
-                    <meta name="keywords" content={pkg.keywords} />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:card" content="summary" />
-                    <meta name="twitter:image" content={lion} />
-                    <meta name="twitter:site" content={site.siteMetadata.author} />
-                    <meta name="twitter:creator" content={site.siteMetadata.author} />
-                    <meta name="twitter:title" content={customSiteTitle || site.siteMetadata.title} />
-                    <meta name="twitter:description" content={site.siteMetadata.description} />
-                  </Helmet>
-                  <GlobalStyles />
-                  {childrenWithSetLanguage}
-                </FullContainer>
-              </ThemeProvider>
-            )}
-          />
-        </Transition>
-      )
-    }
+            }
+          `}
+          render={({ site: { siteMetadata: { title, author, description } } }) => (
+            <ThemeProvider theme={theme}>
+              <FullContainer>
+                <Helmet title={customSiteTitle || title} htmlAttributes={{ lang }}>
+                  <meta name="description" content={pkg.description} />
+                  <meta name="keywords" content={pkg.keywords} />
+                  <meta name="twitter:card" content="summary_large_image" />
+                  <meta name="twitter:card" content="summary" />
+                  <meta name="twitter:image" content={lion} />
+                  <meta name="twitter:site" content={author} />
+                  <meta name="twitter:creator" content={author} />
+                  <meta name="twitter:title" content={customSiteTitle || title} />
+                  <meta name="twitter:description" content={description} />
+                </Helmet>
+                <GlobalStyles />
+                {childrenWithSetLanguage}
+              </FullContainer>
+            </ThemeProvider>
+          )}
+        />
+      </Transition>
+    )
+  }
 }
 
 export default Layout
