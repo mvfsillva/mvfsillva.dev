31921d7852594043fa65e84973d9ab57781b61d0
mvfsilva <mvfsillva@gmail.com>
2019-04-14 18:22:31 -0300

    refactor: adjustment on blog config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,87 @@
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const slugify = require('slugify')
 const _ = require('lodash')
-
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  console.log('*'.repeat(100), actions)
-
-  if(node.iternal.type === 'MarkdownRemark') {
-    const slug = createFilePath(({ node, getNode, basePath: 'posts' }))
-    createNodeField({ node, name: 'slug', value: slug })
-  }
-}
+const path = require('path')
+const slash = require('slash')
 
 exports.createPages = ({ graphql, actions }) => {
-  console.log('*'.repeat(10))
   const { createPage } = actions
-  return new Promise((resolve, _) => {
-    graphql(`
+  const blogPostTemplate = path.resolve('src/templates/single-post.js')
+  const tagPagesTemplate = path.resolve('src/templates/single-tag.js')
+
+  return graphql(
+    `
       {
-        allMarkdownRemark {
+        allMarkdownRemark(filter: { frontmatter: { draft: { ne: true } } }) {
           edges {
-            frontmatter {
-              tags
-            }
-            fields {
-              slug
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                tags
+              }
             }
           }
         }
       }
-    `).then(res => {
-      console.log(res)
-      const posts = res.data.allMarkdownRemark.edges
-      posts.map(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: path.resolve('./src/templates/single-post.js'),
-          context: {
-            slug: node.fields.slug
-          }
-        })
-        let tags = []
-        _.each(posts, edge => {
-          if (_.get(edge, 'node.frontmatter.tags')) {
-            tags = tags.concat(edge.node.frontmatter.tags)
-          }
-        })
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
 
-        tags = _.uniq(tags)
-
-        tags.map(tag => {
-          createPage({
-            path: `/tag/${slugify(tag, { lower: true })}/`,
-            component: path.resolve('./src/templates/tag.js'),
-            context: { tag }
-          })
-        })
+    // Create blog posts pages.
+    result.data.allMarkdownRemark.edges.map(edge => {
+      createPage({
+        path: edge.node.fields.slug, // required
+        component: slash(blogPostTemplate),
+        context: {
+          slug: edge.node.fields.slug,
+          highlight: edge.node.frontmatter.highlight,
+          shadow: edge.node.frontmatter.shadow,
+        },
       })
-      resolve()
+    })
+
+    // Create tag pages.
+    let tags = []
+    result.data.allMarkdownRemark.edges.map(edge => {
+      if (_.get(edge, 'node.frontmatter.tags')) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    tags = _.uniq(tags)
+
+    tags.map(tag => {
+      const tagPath = `/tags/${_.kebabCase(tag)}/`
+      createPage({
+        path: tagPath,
+        component: tagPagesTemplate,
+        context: { tag }
+      })
     })
   })
+}
+
+// Add custom url pathname for blog posts.
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'File') {
+    const parsedFilePath = path.parse(node.absolutePath)
+    const slug = `/${parsedFilePath.dir.split('---')[1]}/`
+    createNodeField({ node, name: 'slug', value: slug })
+  } else if (node.internal.type === 'MarkdownRemark' && typeof node.slug === 'undefined') {
+    const fileNode = getNode(node.parent)
+    createNodeField({
+      node,
+      name: `slug`,
+      value: fileNode.fields.slug,
+    })
+
+    if (node.frontmatter.tags) {
+      const tagSlugs = node.frontmatter.tags.map(tag => `/tags/${_.kebabCase(tag)}/`)
+      createNodeField({ node, name: `tagSlugs`, value: tagSlugs })
+    }
+  }
 }
