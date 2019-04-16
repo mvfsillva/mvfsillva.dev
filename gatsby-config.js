144a253a2cad0ce6c2ef97da39a2219cafe148b8
mvfsilva <mvfsillva@gmail.com>
2019-04-15 22:17:43 -0300

    fix: set basepath to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/`,
+        path: `${__dirname}/src/posts`,
         name: 'posts',
       },
     },
