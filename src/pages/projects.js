10aab9ffd4fb7bf73b37ec5129440db547d58d43
mvfsilva <mvfsillva@gmail.com>
2019-04-17 00:01:23 -0300

    refactor: adjustment component to use Link

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { theme } from 'styled-tools'
+import { transitions } from 'polished'
+
+import { transition } from '../mixins/transition'
 
 import Card from '../components/card'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import Link from '../components/link'
 
 import Content from '../styles/content'
 
@@ -19,14 +23,24 @@ const FlexWrap = styled.div`
   flex-wrap: wrap;
   justify-content: center;
 
-  > div {
+  a {
     width: 40%;
     margin: ${theme('spacing.small')};
     padding: ${theme('spacing')};
+    > div {
+      height: 95%;
+      ${transitions(transition({ property: 'box-shadow', duration: '250ms' }))};
+    }
+    &:hover {
+      > div {
+        box-shadow: ${theme('shadow.large')};
+        ${transitions(transition({ property: 'box-shadow', duration: '250ms' }))};
+      }
+    }
   }
 
-  @media (max-width: 600px) {
-    > div {
+  @media (max-width: 870px) {
+    a {
       width: 100%;
     }
   }
@@ -44,8 +58,10 @@ const Projects = ({ lang, setLanguage }) => {
       <Header navigation={intl.general[lang].navigation} setLanguage={setLanguage} back />
       <Main>
         <FlexWrap>
-          {data.map(({ node: { name, description, repositoryTopics: { nodes } } }) => (
-            <Card key={name} title={name} description={description} tag={nodes} />
+          {data.map(({ node: { name, description, url, repositoryTopics: { nodes } } }) => (
+            <Link href={url} key={name}>
+              <Card title={name} description={description} tag={nodes} />
+            </Link>
           ))}
         </FlexWrap>
       </Main>
