eb60be25969796ff6e44dc594ffacd401f835c76
mvfsilva <marcusvinicius30798@gmail.com>
2019-05-04 22:13:13 +0200

    fix: remove console

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -124,8 +124,6 @@ const Navbar = ({ navigation, back, reverse, setLanguage }) => {
   const [isOpen, setIsOpen] = useState(false)
   const toggle = () => setIsOpen(!isOpen)
 
-  console.log(isOpen)
-
   return (
     <>
       <Nav reverse={reverse} isMobile={isOpen}>
