72c82d69d5c86d73b9bd7481649d71ebd4c8a79b
mvfsilva <marcusvinicius30798@gmail.com>
2019-05-05 21:49:59 -0300

    refactor: blog config improve

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,84 +1,71 @@
 const path = require('path')
-const slash = require('slash')
 const { createFilePath } = require('gatsby-source-filesystem')
+const slugify = require('slugify')
 const _ = require('lodash')
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({ node, getNode, basePath: 'posts' })
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve('src/templates/single-post.js')
-  const tagPagesTemplate = path.resolve('src/templates/single-tag.js')
-
-  return graphql(
-    `
+  return new Promise(resolve => {
+    graphql(`
       {
-        allMarkdownRemark(filter: { frontmatter: { draft: { ne: true } } }) {
+        allMarkdownRemark {
           edges {
             node {
-              fields {
-                slug
-              }
               frontmatter {
                 tags
+              }
+              fields {
+                slug
               }
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+    `).then(result => {
+      const posts = result.data.allMarkdownRemark.edges
+      posts.map(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve('./src/templates/single-post.js'),
+          context: {
+            slug: node.fields.slug,
+          },
+        })
 
-    result.data.allMarkdownRemark.edges.map(edge =>
-      createPage({
-        path: edge.node.fields.slug,
-        component: slash(blogPostTemplate),
-        context: {
-          slug: edge.node.fields.slug,
-          highlight: edge.node.frontmatter.highlight,
-          shadow: edge.node.frontmatter.shadow,
-        },
+        let tags = []
+
+        _.each(posts, edge => {
+          if (_.get(edge, 'node.frontmatter.tags')) {
+            tags = tags.concat(edge.node.frontmatter.tags)
+          }
+        })
+
+        tags = _.uniq(tags)
+
+        tags.map(tag => {
+          createPage({
+            path: `/tag/${slugify(tag, { lower: true })}/`,
+            component: path.resolve('./src/templates/tag.js'),
+            context: {
+              tag,
+            },
+          })
+        })
       })
-    )
-
-    let tags = []
-    result.data.allMarkdownRemark.edges.map(edge => {
-      if (_.get(edge, 'node.frontmatter.tags')) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-
-    tags = _.uniq(tags)
-
-    tags.map(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-      return createPage({
-        path: tagPath,
-        component: tagPagesTemplate,
-        context: { tag }
-      })
+      resolve()
     })
   })
-}
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === 'File') {
-    const slug = createFilePath({ node, getNode, basePath: 'posts' })
-    createNodeField({ node, name: 'slug', value: slug })
-  } else if (node.internal.type === 'MarkdownRemark' && typeof node.slug === 'undefined') {
-    const fileNode = getNode(node.parent)
-    createNodeField({
-      node,
-      name: 'slug',
-      value: fileNode.fields.slug,
-    })
-
-    if (node.frontmatter.tags) {
-      const tagSlugs = node.frontmatter.tags.map(tag => `/tags/${_.kebabCase(tag)}/`)
-      createNodeField({ node, name: 'tagSlugs', value: tagSlugs })
-    }
-  }
 }
