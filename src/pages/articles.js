833525cf5a918e1d8c1f9a7ba6d8f6dc04dfad17
mvfsilva <marcusvinicius30798@gmail.com>
2019-05-05 21:53:33 -0300

    feat: add article page

---
+++ src/pages/articles.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { theme } from 'styled-tools'
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Link from '../components/link'
+
+import getPosts from '../queries/posts'
+
+import contacts from '../helpers/contacts'
+import intl from '../helpers/intl'
+
+import Content from '../styles/content'
+
+const PageTitle = styled.h1`
+  ${theme('typography.title')};
+  margin-bottom: ${theme('spacing.xHuge')};
+  min-width: 800px;
+  text-align: left;
+  span {
+    ${theme('typography.paragraph')};
+  }
+`
+
+const PostList = styled.article`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  align-content: center;
+  padding: ${theme('spacing.small')};
+  margin-bottom: ${theme('spacing.medium')};
+  border-bottom: 1px solid ${theme('palette.black')};
+  min-width: 800px;
+
+  h2 {
+    ${theme('typography.subtitle')};
+  }
+`
+
+const Main = styled(Content)`
+  flex-direction: column;
+  align-items: center;
+`
+
+const Articles = ({ lang, setLanguage }) => {
+  const data = getPosts()
+
+  return (
+    <>
+      <Header navigation={intl.general[lang].navigation} setLanguage={setLanguage} back />
+      <Main>
+        <PageTitle>Articles</PageTitle>
+        {data.edges.map(({ id, node }) => (
+          <Link key={id} to={node.fields.slug}>
+            <PostList>
+              <p>{node.frontmatter.title}</p>
+              <span>{node.frontmatter.date}</span>
+            </PostList>
+          </Link>
+        ))}
+      </Main>
+      <Footer center contacts={contacts} />
+    </>
+  )
+}
+
+Articles.propTypes = {
+  lang: PropTypes.string.isRequired,
+  setLanguage: PropTypes.func.isRequired,
+}
+
+export default Articles
