8497c0400a19fa7d6db3d67f13e9bbc64c7b8a31
mvfsilva <marcusvinicius30798@gmail.com>
2019-05-25 19:15:43 -0300

    refactor: add root file

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Card from './card'
+import Footer from './footer'
+import Header from './header'
+import Link from './link'
+import Loader from './loader'
+import Locale from './locale'
+import Navbar from './navbar'
+import Paragraph from './paragraph'
+import Transition from './transition'
+
+export { Card, Footer, Header, Link, Loader, Locale, Navbar, Paragraph, Transition }
